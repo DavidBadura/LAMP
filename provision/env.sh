64d356cf212bca0c05754d6936096b243b2c483f
DavidBadura <d.a.badura@gmail.com>
2014-05-16 12:46:39 +0200

    update env

--- provision/env.sh
+++ provision/env.sh
@@ -5,9 +5,11 @@ apt-get install -y vim git tree zsh
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
+# oh my zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 chsh -s /bin/zsh vagrant
 
 git clone https://github.com/DavidBadura/.vim.git /home/vagrant/.vim
-bash /home/vagrant/.vim/install.sh
 
 chown vagrant:vagrant /home/vagrant -Rf
