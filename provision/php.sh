e2f1fee90530ce3b01436e709b03b1640003eb59
David Badura <d.badura@gmx.de>
2014-06-11 10:02:26 +0200

    add php5-intl

--- provision/php.sh
+++ provision/php.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get install -y php5 php5-mysql php5-curl php5-xdebug php-pear
+apt-get install -y php5 php5-mysql php5-curl php5-intl php5-xdebug php-pear
 
 
 sed -i.bak 's/;date.timezone =/date.timezone = "Europe\/Berlin"/g' /etc/php5/apache2/php.ini
