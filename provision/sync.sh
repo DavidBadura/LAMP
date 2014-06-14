b33697e5d7572904cbcc3ec802a9bbd77a1841cc
DavidBadura <d.a.badura@gmail.com>
2014-06-14 11:29:02 +0200

    add ssh sync

--- provision/sync.sh
+++ provision/sync.sh
@@ -5,3 +5,8 @@ bash /vagrant/rsymlink.sh /vagrant/files /
 
 echo "symlink /vagrant/local files"
 bash /vagrant/rsymlink.sh /vagrant/local /
+
+echo "ssh"
+rm -Rf /home/vagrant/.ssh
+cp -r /vagrant/ssh /home/vagrant/.ssh
+chmod -Rf 0700 /home/vagrant/.ssh
