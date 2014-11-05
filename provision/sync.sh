4b50e218aa495c3c129e3e08e7ea92f3d5cffd81
DavidBadura <d.badura@gmx.de>
2014-11-05 22:33:44 +0000

    fix ssh sync

--- provision/sync.sh
+++ provision/sync.sh
@@ -6,3 +6,8 @@ cp -Rfv /vagrant/files/* /
 echo "cp /vagrant/local files"
 cp -Rfv /vagrant/local/* /
 
+echo "ssh"
+rm -Rf /home/vagrant/.ssh
+cp -r /vagrant/ssh /home/vagrant/.ssh
+chown -Rf vagrant:vagrant /home/vagrant/.ssh
+chmod -Rf go-rwx,u-x,u+rwX /home/vagrant/.ssh
