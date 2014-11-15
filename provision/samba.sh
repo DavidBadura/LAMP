ec444e4070d9b036ef78323077e6018a75687991
DavidBadura <d.badura@gmx.de>
2014-11-15 12:05:51 +0000

    fix samba create mask

--- provision/samba.sh
+++ provision/samba.sh
@@ -4,12 +4,14 @@ apt-get install -y samba-common samba
 
 echo -ne "vagrant\nvagrant\n" | smbpasswd -a -s vagrant
 
-echo "[www]" 						>> /etc/samba/smb.conf
-echo "	comment = Vagrant LAMP" 	>> /etc/samba/smb.conf
+echo "[www]" 					>> /etc/samba/smb.conf
+echo "	comment = Vagrant LAMP" 		>> /etc/samba/smb.conf
 echo "	path = /var/www" 			>> /etc/samba/smb.conf
-echo "	valid users = vagrant" 		>> /etc/samba/smb.conf
+echo "	valid users = vagrant" 			>> /etc/samba/smb.conf
 echo "	public = no" 				>> /etc/samba/smb.conf
 echo "	writable = yes" 			>> /etc/samba/smb.conf
 echo "	printable = no" 			>> /etc/samba/smb.conf
+echo "	directory mask = 755" 			>> /etc/samba/smb.conf
+echo "	create mask = 664" 			>> /etc/samba/smb.conf
 
 service samba restart
