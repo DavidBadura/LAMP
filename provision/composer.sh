c8155e45920ad77a1b5d13c8cf7708bd723cf21c
David Badura <david.badura@i22.de>
2015-11-25 15:27:36 +0100

    update

--- provision/composer.sh
+++ provision/composer.sh
@@ -3,7 +3,6 @@
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
-composer global require 'phpunit/phpunit=*' 'fabpot/php-cs-fixer=*' 'instaclick/php-code-sniffer=*' 'sensiolabs/security-checker=*'
-composer global update
+composer global require 'fabpot/php-cs-fixer=*' 'instaclick/php-code-sniffer=*' 'sensiolabs/security-checker=*'
 
 mv /root/.composer /home/vagrant/.composer
--- provision/nodejs.sh
+++ provision/nodejs.sh
@@ -4,8 +4,10 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | b
 
 source /root/.nvm/nvm.sh
 nvm install 5
+nvm alias default node
 
 mv /root/.nvm /home/vagrant/.nvm
 
 echo 'export NVM_DIR="/home/vagrant/.nvm"' >> /home/vagrant/.zshrc
 echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> /home/vagrant/.zshrc
+#echo 'nvm use stable' >> /home/vagrant/.zshrc
